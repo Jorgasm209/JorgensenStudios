506cfc6480f1d275f5feb7ec0811a5b599fa66fa
Brian Jorgensen <Brian.Jorgensen@unifac.com>
2025-05-12 19:56:09 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Tribute Album</h1>
+      <p>This is a celebration of our love and the moments that make it special.</p>
+      {/* Add more content and components here */}
+    </div>
+  );
+}
+
+export default App;
